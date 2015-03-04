8ea9895a6335917227852938328e07c8f61a3f12
jaketrent <trent.jake@gmail.com>
2015-03-04 07:44:40 -0700

    react component renders

--- test/unit/timesheet.spec.js
+++ test/unit/timesheet.spec.js
@@ -1,5 +1,27 @@
+var React = require('react/addons');
+var should = require('should');
+
+var Timesheet = require('../../lib/timesheet');
+
+var TestUtils = React.addons.TestUtils;
+
 describe('awesome', function () {
-  it('is', function () {
-    true.should.be.true;
+
+  it('is a function', function () {
+    Timesheet.should.be.an.instanceOf(Function);
   });
+  var timesheet;
+  beforeEach(function () {
+    timesheet = TestUtils.renderIntoDocument(Timesheet())
+  });
+
+  it('renders', function () {
+    timesheet.isMounted().should.be.true;
+  });
+
+  it('is contained in .timesheet', function () {
+    var div = TestUtils.findRenderedDOMComponentWithClass(timesheet, 'timesheet');
+    should.exist(div);
+  });
+
 });
