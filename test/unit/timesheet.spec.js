2a91fa483f238faf15a1b17e829136bcb6d3fdfb
jaketrent <trent.jake@gmail.com>
2015-03-07 13:04:12 -0700

    render scale

--- test/unit/timesheet.spec.js
+++ test/unit/timesheet.spec.js
@@ -1,3 +1,4 @@
+var range = require('lodash-node/modern/utility/range');
 var React = require('react/addons');
 var should = require('should');
 
@@ -6,13 +7,18 @@ var Timesheet = require('../../lib/timesheet');
 var TestUtils = React.addons.TestUtils;
 
 describe('awesome', function () {
+  var min = 2002;
+  var max = 2020;
+  var timesheet;
+  beforeEach(function () {
+    timesheet = TestUtils.renderIntoDocument(Timesheet({
+      min: min,
+      max: max
+    }));
+  });
 
   it('is a function', function () {
     Timesheet.should.be.an.instanceOf(Function);
-  });
-  var timesheet;
-  beforeEach(function () {
-    timesheet = TestUtils.renderIntoDocument(Timesheet())
   });
 
   it('renders', function () {
@@ -22,6 +28,31 @@ describe('awesome', function () {
   it('is contained in .timesheet', function () {
     var div = TestUtils.findRenderedDOMComponentWithClass(timesheet, 'timesheet');
     should.exist(div);
+  });
+
+  describe('scale', function () {
+    it('takes a min year prop', function () {
+      timesheet.props.min.should.eql(min);
+    });
+
+    it('takes a max year prop', function () {
+      timesheet.props.max.should.eql(max);
+    });
+
+    it('renders a child for every year', function () {
+      var scale = TestUtils.findRenderedDOMComponentWithClass(timesheet, 'scale');
+      should.exist(scale);
+      scale.props.children.length.should.eql(max - min);
+    });
+
+    it('renders each year in order from min to max', function () {
+      var scale = TestUtils.findRenderedDOMComponentWithClass(timesheet, 'scale');
+      var children = [].slice.call(scale.getDOMNode().children);
+      var years = range(min, max);
+      children.forEach(function (child, i) {
+        child.innerText.should.eql(years[i].toString());
+      });
+    });
   });
 
 });
